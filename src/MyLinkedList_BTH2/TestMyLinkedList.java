ac0a3973710e2547831456f56dd3c61af6749c18
Lê Thế Anh <letheanhbn2003@gmail.com>
2023-03-22 08:29:40 +0700

    Update_BT

--- src/MyLinkedList_BTH2/TestMyLinkedList.java
+++ src/MyLinkedList_BTH2/TestMyLinkedList.java
@@ -3,13 +3,13 @@ package MyLinkedList_BTH2;
 public class TestMyLinkedList {
     public static void main(String[] args) {
         System.out.println("/=/=/=/= TESTING /=/=/=/=");
-        MyLinkedList ll = new MyLinkedList(10);
-        ll.addFirst(11);
-        ll.addFirst(12);
-        ll.addFirst(13);
+        MyLinkedList addFirst = new MyLinkedList(10);
+        addFirst.addFirst(11);
+        addFirst.addFirst(12);
+        addFirst.addFirst(13);
 
-        ll.add(4,9);
-        ll.add(4,9);
-        ll.printList();
+        addFirst.add(4,9);
+        addFirst.add(4,9);
+        addFirst.printList();
     }
 }
--- src/MyList_BTH1/MyList.java
+++ src/MyList_BTH1/MyList.java
@@ -23,6 +23,7 @@ public class MyList<E> {
         elements[size++] = e;
     }
 
+
     public E get(int i) {
         if (i>= size || i <0) {
             throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
